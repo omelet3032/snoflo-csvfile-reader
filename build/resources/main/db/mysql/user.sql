75ed763b7c2d96d8973a837d38334ad23c558123
omelet3032 <omelet3032@gmail.com>
2024-10-03 00:37:05 +0900

    gitignore 수정 -> test폴더 제거

--- build/resources/main/db/mysql/user.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE snoflo_file_reader;
--- CREATE USER 'snoflo'@'localhost' IDENTIFIED BY 'Snoflo123!!';
-CREATE USER 'snoflo'@'%' IDENTIFIED BY 'Snoflo123!!';
-GRANT ALL PRIVILEGES ON snoflo_file_reader.* TO 'snoflo'@'%';
-FLUSH PRIVILEGES;
