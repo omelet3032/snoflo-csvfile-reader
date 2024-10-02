7ac5846977b4e3452c303d0e1a9f323584c9372c
omelet3032 <omelet3032@gmail.com>
2024-10-02 12:34:41 +0900

    CsvFileReader 수정

--- src/main/java/org/snoflo/function/CsvFileReader.java
+++ src/main/java/org/snoflo/function/CsvFileReader.java
@@ -9,17 +9,55 @@ import java.util.List;
 
 public class CsvFileReader {
 
-	public List<String[]> readCsvFile(Path selectedFile) {
+	private List<Question> list;
 
-		List<String[]> list = new ArrayList<>();
+	public CsvFileReader(Question question) {
+		this.list = question.getList(); 
+	}
+
+	public List<Question> readCsvFile(Path selectedFile) {
 
 		try (BufferedReader reader = Files.newBufferedReader(selectedFile)) {
-			String line;
-			reader.readLine(); // 첫 줄 없애기
+			String line = reader.readLine();
+
+			String concept = "";
+			String description = "";
+			StringBuilder descriptionBuilder = new StringBuilder();
 
 			while ((line = reader.readLine()) != null) {
+
 				String[] values = line.split(",");
-				list.add(values);
+
+				descriptionBuilder.setLength(0);
+
+				if (line.contains("\"")) {
+
+					if (isQuoteMiddleIndex(line)) {
+
+						concept = values[0];
+						description = appendDescriptionColumn(descriptionBuilder, values);
+
+						addQuestionList(concept, description);
+
+					} else {
+						
+						Question question = list.getLast();
+
+						description = appendDescriptionColumn(question, descriptionBuilder, values);
+
+						question.setDescription(description);
+
+						this.list.set(list.size() - 1, question);
+
+					}
+
+				} else {
+					concept = values[0];
+					description = values[1];
+
+					addQuestionList(concept, description);
+				}
+
 			}
 
 		} catch (IOException e) {
@@ -27,7 +65,53 @@ public class CsvFileReader {
 
 		}
 
-		return list;
+		return this.list;
 	}
 
+	private void addQuestionList(String concept, String description) {
+		Question question = new Question(concept, description);
+		this.list.add(question);
+	}
+
+	private boolean isQuoteMiddleIndex(String line) {
+		int quoteIndex = line.indexOf("\"");
+		int lastIndex = line.length() - 1;
+
+		return quoteIndex != lastIndex;
+	}
+
+	private String appendDescriptionColumn(
+			Question question,
+			StringBuilder descriptionBuilder,
+			String[] values) {
+
+		String beforeDescription = question.getDescription();
+		descriptionBuilder = descriptionBuilder.append(beforeDescription).append(", ");
+
+		return appendDescriptionColumn(descriptionBuilder, values);
+	}
+
+	private String appendDescriptionColumn(
+			StringBuilder descriptionBuilder,
+			String[] values) {
+
+		int startIndex = (descriptionBuilder.length() == 0) ? 1 : 0;
+
+		for (int i = startIndex; i < values.length; i++) {
+
+			if (values[i].contains("\"")) {
+				values[i] = values[i].replaceAll("\"", "");
+			}
+
+			descriptionBuilder.append(values[i]);
+
+			if (i != values.length - 1) {
+				descriptionBuilder.append(",");
+			}
+
+		}
+
+		return descriptionBuilder.toString();
+
+	}
 }
