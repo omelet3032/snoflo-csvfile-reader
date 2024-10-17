52e8b3b119b83727a67663cf45737ad61c792fb3
omelet3032 <omelet3032@gmail.com>
2024-10-17 18:58:16 +0900

    팩토리 메서드 클래스 제거

--- src/main/java/org/snoflo/application/Application.java
+++ src/main/java/org/snoflo/application/Application.java
@@ -1,14 +1,9 @@
 package org.snoflo.application;
 
-import java.io.IOException;
-import java.util.Scanner;
-
 import org.snoflo.controller.AppController;
 import org.snoflo.controller.EntryController;
 import org.snoflo.controller.FinderController;
 import org.snoflo.controller.QuestionController;
-import org.snoflo.factory.FinderFactory;
-import org.snoflo.factory.QuestionFactory;
 
 public class Application {
 
@@ -16,15 +11,11 @@ public class Application {
 
         ResourceManager resourceManager = new ResourceManager();
 
-        // FinderController finderController = (FinderController) new FinderFactory().createOperation(resourceManager);
-        // QuestionController questionController = (QuestionController) new QuestionFactory()
-        //         .createOperation(resourceManager);
+        FinderFactory finderFactory = new FinderFactory();
+        FinderController finderController = finderFactory.getFinderController(resourceManager);
 
-        FinderFactory2 finderController2 = new FinderFactory2();
-        finderController2.getFinderController(resourceManager);
-
-        QuestionFactory2 questionController2 = new QuestionFactory2();
-        questionController2.getQuestionController(resourceManager);
+        QuestionFactory questionFactory = new QuestionFactory();
+        QuestionController questionController = questionFactory.getQuestionController(resourceManager);
 
         AppController entryController = new EntryController(resourceManager, finderController, questionController);
 
